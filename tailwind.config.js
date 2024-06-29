c60f8ee5d6e2217d3f3bf4378d13c1774c08b6c5
Narain Singaram <narainsingaram7@gmail.com>
2024-06-29 10:41:21 -0400

    bro what

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' if you prefer
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('daisyui'),
   ],
 };
