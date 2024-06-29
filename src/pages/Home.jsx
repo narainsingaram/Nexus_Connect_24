21f586f787e5a79323e69690920757a9383c32f7
Narain Singaram <narainsingaram7@gmail.com>
2024-06-29 01:17:02 -0400

    fixing issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -439,16 +439,24 @@ const Home = () => {
      ))}
   </div>
   <div class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-neutral-700 dark:divide-neutral-700">
-        <span class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" onClick={() => handleModal(item)} href="#">
+        <span class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-es-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"onClick={() => handleModal(item)} loading={loading} href="#">
           View
         </span>
+        {adminMode && isAdminModeValid() && (
         <span class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" onClick={() => navigate(`/update/${item.id}`)} href="#">
           Update
         </span>
+        )}
+        {adminMode && isAdminModeValid() && (
         <span class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-ee-xl bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800" onClick={() => handleDelete(item.id)} href="#">
           Delete
         </span>
+        )}
       </div>
+      {open && (
+        <ModalComp open={open} setOpen={setOpen} handleDelete={handleDelete} {...user} />
+    )}
+
 </section>
     </div>
 
